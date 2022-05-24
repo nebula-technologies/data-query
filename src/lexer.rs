4f43da01824978eed06538a6e1d1d0cddacb44ba
Anders Blenstrup-Pedersen <abp-git@ryuu.technology>
2022-05-24 20:17:00 +0900

    update - adding more to the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,43 +1,98 @@
 use crate::lexer_constants::*;
+use std::num::ParseIntError;
 
-pub struct Identifier {
-    name: Option<String>,
-    generic: Option<GenericObjectIndex>,
+pub enum LexerError {
+    EndOfQuery { expected: String },
+    FailedToParseInt(ParseIntError),
 }
+
+impl From<ParseIntError> for LexerError {
+    fn from(e: ParseIntError) -> Self {
+        Self::FailedToParseInt(e)
+    }
+}
+
+pub type LexResult<T> = Result<T, LexerError>;
 
 pub enum GenericObjectIndex {
     Wildcard,
-    Slice(Vec<Slicer>)
+    Slice(Vec<Slicer>),
 }
 
 pub enum Slicer {
     Index(usize),
-    Slice(usize,usize),
-    Ident(String)
+    SliceFrom(usize),
+    SliceTo(usize),
+    Ident(String),
 }
 
-pub enum LexOperator{
-    Identifier(Identifier),
+pub enum LexOperator {
+    Identifier(String),
     Pipe(Vec<LexOperator>),
+    Generic(GenericObjectIndex),
 }
-
 
 pub fn compile(s: &str) -> Vec<LexOperator> {
+    let lexer_vec = s.chars().into_iter().collect::<Vec<char>>();
+    let mut lex = Vec::new();
     let mut last = "";
-    for c in s.chars() {
-        match c {
-            LEX_IDENTIFIER => {
-
-            }
-            _ => {
-
-            }
-        }
-    }
+    let mut generic = "";
 
     Vec::new()
 }
 
+pub fn generic_compiler(
+    mut lexer_vec: &Vec<char>,
+    mut collect: String,
+) -> LexResult<Vec<LexOperator>> {
+    let char = lexer_vec.pop();
+    let mut lex_complied = Vec::new();
+    if let Some(c) = char {
+        match c {
+            LEX_IDENTIFIER => {
+                lex_complied.push(LexOperator::Identifier(collect.to_string()));
+                collect = Default::default();
+            }
+            LEX_GENERIC_START => {
+                let v = generic_object_index(lexer_vec, Default::default(), Vec::new())?;
+                lex_complied.push(LexOperator::Generic(v));
+            }
+            _ => collect.push(c),
+        }
+    }
+
+    Ok(lex_complied)
+}
+
+fn generic_object_index(
+    mut lexer_vec: &Vec<char>,
+    collect: String,
+    mut slicer: Vec<Slicer>,
+) -> LexResult<GenericObjectIndex> {
+    let char = lexer_vec.pop();
+    if let Some(c) = char {
+        match c {
+            LEX_GENERIC_END => {
+                if collect.is_empty() && slicer.is_empty() {
+                    Ok(GenericObjectIndex::Wildcard)
+                } else if !collect.is_empty() {
+                    if let Some(Slicer::SliceFrom(u)) = slicer.last() {
+                        let slice = collect.parse::<usize>().map_err(LexerError::from)?;
+                        slicer.push(Slicer::SliceTo(slice))
+                    }
+                    Ok(GenericObjectIndex::Slice(slicer))
+                } else {
+                    Ok(GenericObjectIndex::Slice(slicer))
+                }
+            }
+            _ => {}
+        }
+    } else {
+        Err(LexerError::EndOfQuery {
+            expected: String::from(LEX_GENERIC_END),
+        })
+    }
+}
 
 /*
 
--- src/lexer_constants.rs
+++ src/lexer_constants.rs
@@ -7,5 +7,9 @@ pub(crate) const BLOCK_PROCEED: &str = "\u{0}\u{0}\u{0}\u{0}\u{0}\u{7}\u{0}\u{0}
 
 pub(crate) const LEX_IDENTIFIER: char = '.';
 pub(crate) const LEX_PIPE: char = '|';
+
 pub(crate) const LEX_CAPSULE_START: char = '(';
 pub(crate) const LEX_CAPSULE_END: char = ')';
+
+pub(crate) const LEX_GENERIC_START: char = '[';
+pub(crate) const LEX_GENERIC_END: char = '[';
